c7357a09124933f89a9fa97705846e1c69425148
DocErnstz <ernesto800@outlook.com>
2021-08-10 17:47:26 -0300

    navbar scrollTop class functions

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
+$.ScrollBar = function (Section) {
+  this.lastPos = 0;
+  this.comingBack = false;
+  this.Section = Section;
+};
+$.ScrollBar.prototype = {
+  checkComing: function () {
+    if (
+      this.lastPos > this.Section.scrollTop() &&
+      this.Section.scrollTop() != 0
+    ) {
+      this.comingBack = true;
+    } else {
+      this.comingBack = false;
+    }
+  },
+  setLastPos: function () {
+    this.lastPos = this.Section.scrollTop();
+  },
+};
+var scrollbar = new $.ScrollBar($("#mainBar"));
+
 $("#mainBar").on("scroll", function () {
-  var scrollPos = $("#mainBar").scrollTop();
-  if (scrollPos > 40) {
+  scrollbar.checkComing();
+  scrollbar.setLastPos();
+
+  if (scrollbar.Section.scrollTop() > 40) {
     $(".navbar").addClass("shadownav");
   } else {
     $(".navbar").removeClass("shadownav");
+  }
+  if (scrollbar.comingBack) {
+    console.log("0");
+    $(".navbar").removeClass("shadownav");
+    $(".navbar").addClass("comeback");
+  } else {
+    $(".navbar").removeClass("comeback");
   }
 });
