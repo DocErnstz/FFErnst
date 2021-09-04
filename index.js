a7d47ce9185a62f9b674bdddee934bd2e21caf87
DocErnstz <ernesto800@outlook.com>
2021-09-04 03:48:53 -0300

    texts replaced

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-$.ScrollBar = function (Section, steps) {
+$.ScrollBar = function (Section, steps, visitedSteps) {
   this.lastPos = 0;
   this.comingBack = false;
   this.Section = Section;
   this.steps = steps;
+  this.visitedSteps = visitedSteps;
 };
 $.ScrollBar.prototype = {
   checkComing: function () {
@@ -19,20 +20,51 @@ $.ScrollBar.prototype = {
     this.lastPos = this.Section.scrollTop();
   },
 };
-var scrollbar = new $.ScrollBar($("#mainBar"), {
-  450: "#AboutMe",
-  950: "#title",
-  1150: "#Project1",
-  1550: "#Project2",
-  1950: "#Project3",
-  2350: "#Project4",
-});
+var scrollbar = new $.ScrollBar(
+  $("#mainBar"),
+  {
+    450: "#AboutMe",
+    950: "#title",
+    1150: "#Project1",
+    1550: "#Project2",
+    1950: "#Project3",
+    2350: "#Project4",
+    2550: "#titleMore1",
+    2650: "#titleMore2",
+    3150: "line 1",
+    3450: "line 2",
+    3850: "line 3",
+  },
+  []
+);
 
-$("#mainBar").on("scroll", function () {
+scrollbar.Section.on("scroll", function () {
   scrollbar.checkComing();
   scrollbar.setLastPos();
   Object.keys(scrollbar.steps).forEach((step) => {
     if (
+      scrollbar.Section.scrollTop() > step &&
+      scrollbar.steps[step].split(" ").length > 1 &&
+      !scrollbar.visitedSteps.includes(scrollbar.steps[step].split(" ")[1])
+    ) {
+      $(
+        `.sideprojects > li:nth-child(${
+          (parseInt(scrollbar.steps[step].split(" ")[1]) - 1) * 3 + 1
+        }`
+      ).addClass("showUp");
+      $(
+        `.sideprojects > li:nth-child(${
+          (parseInt(scrollbar.steps[step].split(" ")[1]) - 1) * 3 + 2
+        })`
+      ).addClass("showUp");
+
+      $(
+        `.sideprojects > li:nth-child(${
+          (parseInt(scrollbar.steps[step].split(" ")[1]) - 1) * 3 + 3
+        })`
+      ).addClass("showUp");
+      scrollbar.visitedSteps.push(scrollbar.steps[step].split(" ")[1]);
+    } else if (
       scrollbar.Section.scrollTop() > step &&
       !$(scrollbar.steps[step]).hasClass("showUp")
     ) {
@@ -51,4 +83,9 @@ $("#mainBar").on("scroll", function () {
   } else {
     $(".navbar").removeClass("comeback");
   }
+});
+
+$(".sideprojects > li").mouseover(function () {
+  $(this).removeClass("showUp");
+  $(this).css({ opacity: "1" });
 });
